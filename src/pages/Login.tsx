4e55027ce045a64ca48f8c7113b4e9d78f6c055c
daichisakurai <daichisakurai.0815@gmail.com>
2022-05-28 13:16:30 +0900

    fix:remember meチェックボックス削除

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,8 +4,6 @@ import Avatar from '@mui/material/Avatar'
 import Button from '@mui/material/Button'
 import CssBaseline from '@mui/material/CssBaseline'
 import TextField from '@mui/material/TextField'
-import FormControlLabel from '@mui/material/FormControlLabel'
-import Checkbox from '@mui/material/Checkbox'
 import Box from '@mui/material/Box'
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
 import Typography from '@mui/material/Typography'
@@ -158,10 +156,6 @@ const Login: React.VFC = () => {
               autoComplete="current-password"
               helperText={alertPasswordText}
               error={errorPasswordInput}
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
             />
             <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
               Sign In
